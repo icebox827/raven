6dcefbfc35389ee77e9d5627e72bcfd6c1fa6ebe
Denis Lafontant <icebox827@gmail.com>
2021-02-26 17:19:17 -0500

    last update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_171527) do
+ActiveRecord::Schema.define(version: 2021_02_26_020305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,9 @@ ActiveRecord::Schema.define(version: 2021_02_26_171527) do
   create_table "friendships", force: :cascade do |t|
     t.integer "creator_id"
     t.integer "receiver_id"
-    t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status"
     t.index ["creator_id"], name: "index_friendships_on_creator_id"
     t.index ["receiver_id"], name: "index_friendships_on_receiver_id"
   end
