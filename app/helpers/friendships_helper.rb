276869ee51d54d7609444538a8342acd0eab6c4a
Denis Lafontant <icebox827@gmail.com>
2021-02-28 19:18:15 -0500

    Fix invite button

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,7 +1,2 @@
 module FriendshipsHelper
-  def confirm_a_request(user)
-    @friendship = current_user.receiver.find { |friendship| friendship.user == user }
-    @friendship.status = true
-    @friendship.save
-  end
 end
