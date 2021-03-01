f10bed13dbc6487f38350bb32c0b24d7d89dc659
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-01 14:31:53 -0600

    List all users in list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,14 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    #@users = User.joins('INNER JOIN friendships ON users.id=creator_id').where('friendships.creator_id != users.id or friendships.receiver_id != users.id')
+    #@fri = Friendship.select(:receiver_id).where("creator_id = ?", current_user)
+    #@fri2 = Friendship.select(:creator_id).where("receiver_id = ?", current_user)
+    #@users = User.where("id != ?", current_user).where("id NOT IN", Friendship.select(creator_id).where(:receiver_id current_user))
     # @attendees = User.joins('INNER JOIN invitations ON users.user_id=invitations.attendee').where('invitations.attended_event' => @event.id)
+    #@users = User.where("id != ?", current_user)
+    #@users.each do |user|
+      #@future_friends = user.id unless Friendship.where(:creator_id current_user, :receiver_id user.id).or(:creator_id user.id, :receiver_id current_user)
+    #end
   end
 
   def show
