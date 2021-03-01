d3d17e13873e4464ce1bd8529428dbfab0a59abf
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-01 13:37:19 -0600

    Fixing destroy button and friendship creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    #@users = User.joins('INNER JOIN friendships ON users.id=creator_id').where('friendships.creator_id != users.id or friendships.receiver_id != users.id')
+    # @attendees = User.joins('INNER JOIN invitations ON users.user_id=invitations.attendee').where('invitations.attended_event' => @event.id)
   end
 
   def show
