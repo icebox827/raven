19775a5f8ad1bdef908414d3af0243cf815449d4
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-02 12:46:20 -0600

    Installing rspec-rails, replacing to rspec and creating the first test method

--- Gemfile
+++ Gemfile
@@ -46,7 +46,7 @@ group :development, :test do
 end
 
 group :test do
-  gem 'rspec'
+  gem 'rspec-rails'
 end
 
 group :development do
---
+++ spec/friendship_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Friendship', type: :model do
+  it 'this will create a new friendship' do
+    user1 = User.create!(email: 'test1@email.com', name: 'test1', gravatar_url: 'http://www.gravatar.com/avatar/%22',
+                        password: '123123')
+    user2 = User.create!(email: 'test2@email.com', name: 'test2', gravatar_url: 'http://www.gravatar.com/avatar/%22',
+                        password: '123123')
+    f = Friendship.create!(creator_id: user1.id, receiver_id: user2.id, status: false)
+    expect(Friendship.find(f.id).id).to eql(Friendship.last.id)
+  end
+end
