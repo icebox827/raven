b6cec689557ef5b633c0a4116d3fa7fd91a60462
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-03-02 13:10:23 -0600

    Making changes on rspec test

--- spec/friendship_spec.rb
+++ spec/friendship_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'Friendship', type: :model do
-  it 'this will create a new friendship' do
-    user1 = User.create!(email: 'test1@email.com', name: 'test1', gravatar_url: 'http://www.gravatar.com/avatar/%22',
-                         password: '123123')
-    user2 = User.create!(email: 'test2@email.com', name: 'test2', gravatar_url: 'http://www.gravatar.com/avatar/%22',
-                         password: '123123')
-    f = Friendship.create!(creator_id: user1.id, receiver_id: user2.id, status: false)
-    expect(Friendship.find(f.id).id).to eql(Friendship.last.id)
+describe Friendship do
+  describe '#create_friendship' do
+    it 'this will create a new friendship' do
+      user1 = User.create!(email: 'test1@email.com', name: 'test1', gravatar_url: 'http://www.gravatar.com/avatar/%22',
+                           password: '123123')
+      user2 = User.create!(email: 'test2@email.com', name: 'test2', gravatar_url: 'http://www.gravatar.com/avatar/%22',
+                           password: '123123')
+      f = Friendship.create!(creator_id: user1.id, receiver_id: user2.id, status: false)
+      expect(Friendship.find(f.id).id).to eql(Friendship.last.id)
+    end
   end
 end
