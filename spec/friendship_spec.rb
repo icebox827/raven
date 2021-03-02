5112f7e6074750897564c6898e444090fb7e619f
Denis Lafontant <icebox827@gmail.com>
2021-03-02 15:37:16 -0500

    Create test case 2 for firndship

--- spec/friendship_spec.rb
+++ spec/friendship_spec.rb
@@ -11,4 +11,16 @@ describe Friendship do
       expect(Friendship.find(f.id).id).to eql(Friendship.last.id)
     end
   end
+
+  describe '#accept_friendship' do
+    it 'This will accept a friendship' do
+      user1 = User.create!(email: 'test1@email.com', name: 'test1', gravatar_url: 'http://www.gravatar.com/avatar/%22',
+                           password: '123123')
+      user2 = User.create!(email: 'test2@email.com', name: 'test2', gravatar_url: 'http://www.gravatar.com/avatar/%22',
+                           password: '123123')
+
+      f = Friendship.create!(creator_id: user1.id, receiver_id: user2.id, status: true)
+      expect(Friendship.find(f.id).id).to eql(Friendship.last.id)
+    end
+  end
 end
