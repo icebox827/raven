9ba2bbfd58ad9cccb36d13918403fb45b61f8a45
Denis Lafontant <icebox827@gmail.com>
2021-03-02 16:03:19 -0500

    Add last test

--- spec/friendship_spec.rb
+++ spec/friendship_spec.rb
@@ -19,8 +19,21 @@ describe Friendship do
       user2 = User.create!(email: 'test2@email.com', name: 'test2', gravatar_url: 'http://www.gravatar.com/avatar/%22',
                            password: '123123')
 
-      f = Friendship.create!(creator_id: user1.id, receiver_id: user2.id, status: true)
+      f = Friendship.create(creator_id: user1.id, receiver_id: user2.id, status: false)
+      Friendship.update(status: true)
       expect(Friendship.find(f.id).id).to eql(Friendship.last.id)
+    end
+  end
+
+  describe '#reject_friendship' do
+    it 'This will reject a frienship' do
+      user1 = User.create!(email: 'test1@email.com', name: 'test1', gravatar_url: 'http://www.gravatar.com/avatar/%22',
+                           password: '123123')
+      user2 = User.create!(email: 'test2@email.com', name: 'test2', gravatar_url: 'http://www.gravatar.com/avatar/%22',
+                           password: '123123')
+
+      f = Friendship.create!(creator_id: user1.id, receiver_id: user2.id, status: false)
+      Friendship.destroy(Friendship.find(f.id).id)
     end
   end
 end
